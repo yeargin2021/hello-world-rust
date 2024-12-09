82558f11d5dc98cf74ec229c78065bc915686173
Tommy H. Yeargin, Jr <88904457+yeargin2021@users.noreply.github.com>
2024-12-09 06:58:57 -0500

    commit

--- src/io_operations.rs
+++ src/io_operations.rs
@@ -1,15 +1,15 @@
 use chrono::Local;
 use std::fs::File;
-use std::io::{self, Write}; // Import Write to use write! macro
+use std::io::Write; // Import Write to use write! macro
 
-fn main() -> io::Result<()> {
-    let mut file = File::create("output.txt")?; // Try to create or open file
-
+fn main() -> std::io::Result<()> {
+    let mut file = File::create("output.txt")?; // Create or open output.txt
+    
     let header = "Tommy H. Yeargin, Jr.\n1014 Circle Rd.\nEasley, SC 29642";
     println!("{}", header);
     write!(file, "{}\n", header)?; // Write header to file
 
-    let today = Local::now();
+    let today = Local::today();
     let date_str = format!("\n{}", today.format("%d %B %Y"));
     print!("{}", date_str);
     write!(file, "{}", date_str)?; // Write date to file
@@ -32,6 +32,6 @@ tyjr
 "#;
     println!("{}", recipient_address);
     write!(file, "{}", recipient_address)?; // Write address and message to file
-
+    
     Ok(())
 }
